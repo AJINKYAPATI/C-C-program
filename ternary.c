c8566b8eb91d3b0869aa287d4fad8bb4ddfd1d30
AJINKYAPATI <158836219+AJINKYAPATI@users.noreply.github.com>
2025-09-27 13:19:25 +0530

    Create ternary.c

---
+++ ternary.c
@@ -0,0 +1,22 @@
+  #include<stdio.h>
+int main()
+{
+    int num1,num2,num3;
+    printf("enter the first number :");
+    scanf("%d",&num1);
+
+    printf("enter the second number :");
+    scanf( "%d", &num2);
+
+    
+    printf("enter the third number :");
+    scanf("%d",&num3);
+
+    int max=num1>num2?num1:num2;
+        max=max>num3?max:num3;
+
+    printf("max num is %d",max);
+
+}
+
+
