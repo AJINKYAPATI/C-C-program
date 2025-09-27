536503f7da1a9caee468cb1cd30fbc9c3f789bd1
AJINKYAPATI <158836219+AJINKYAPATI@users.noreply.github.com>
2025-09-27 13:21:20 +0530

    Create type of character.c

---
+++ type of character.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<ctype.h>
+int main(){
+
+    char ch;
+    printf("enter the character: ");
+    scanf("%c",&ch);
+
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || 
+          ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+            printf("character is vowel, %c", ch);
+          } else if (isalpha(ch)) {
+            printf("character is cnsonant, %c", ch);
+          } else if (isdigit(ch)) {
+            printf("character is digit, %c", ch);
+          } else {
+            printf("character is special symbol, %c", ch);
+          }
+
+          return 0;
+}
