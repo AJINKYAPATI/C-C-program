f304ab632224558ffea5782e4b014da45fc0135c
AJINKYAPATI <158836219+AJINKYAPATI@users.noreply.github.com>
2025-10-09 00:30:43 +0530

    Create array max.c

---
+++ array max.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    
+    int n;
+    printf("enter a size for array:");
+    scanf("%d",&n);
+    int arr[n];
+
+    printf("enter value for array:");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int max=arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    printf("max :%d",max);
+
+    return 0;
+}
