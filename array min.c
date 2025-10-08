eff901d536d103fea7fe7d1aa8873c7e4de0c6d5
AJINKYAPATI <158836219+AJINKYAPATI@users.noreply.github.com>
2025-10-09 00:32:28 +0530

    Create array min.c

---
+++ array min.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    
+    int n;
+    printf("enter a size for array:");
+    scanf("%d",&n);
+    int arr[n];
+
+    printf("enter value for array:");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int min=arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    printf("min :%d",min);
+
+    return 0;
+}
