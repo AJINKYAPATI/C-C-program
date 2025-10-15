9ba6e387540313e5e926b1bde8f72072af50e00a
AJINKYAPATI <158836219+AJINKYAPATI@users.noreply.github.com>
2025-10-15 23:32:04 +0530

    Add program to check if a number is even or odd

---
+++ nested2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+    int num;
+    printf("enter one num");
+    scanf("%d",&num);
+
+    if(num>=0){
+        if(num%2==0){
+        printf("num is even");
+        }else{
+            printf("num is odd");
+        }
+    }else{
+        printf("num is negative");
+    }
+}
