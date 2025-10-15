ddef2849b2e7ada23a4228f7de32f4d72984c1c0
AJINKYAPATI <158836219+AJINKYAPATI@users.noreply.github.com>
2025-10-15 23:32:46 +0530

    Add character classification for uppercase letters
    Check if the entered character is an uppercase vowel or consonant.

---
+++ nested3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+    char ch;
+    printf("enter the alphabet: ");
+    scanf("%c",&ch);
+
+    if(ch >= 'A' && ch <= 'z'){
+        if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'U'){
+            printf("uppercase vowel: %c",ch);
+        }else {
+        printf("uppercase consonant: %c",ch);
+        }
+     } else {
+        printf("the entered character is not an uppercase");
+    }
+}
