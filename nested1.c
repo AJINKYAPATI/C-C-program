0749852de772f8efd16965f0fbea333dcd08c348
AJINKYAPATI <158836219+AJINKYAPATI@users.noreply.github.com>
2025-10-15 23:31:06 +0530

    Add multiplication table program in nested1.c
    Implement multiplication table for user-defined range

---
+++ nested1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+
+    int a=1,n,b=1;
+    printf("enter a number:");
+    scanf("%d",&n);
+
+    while(a<=10){
+        b=1;
+        while(b<=n){
+            printf("%d\t",a*b);
+            b++;
+        }
+        printf("\n");
+        a++;
+    }
+
+    return 0;
+}
