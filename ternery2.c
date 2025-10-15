7a367b85f9f74beca14c0ed7fe07d86b011a8344
AJINKYAPATI <158836219+AJINKYAPATI@users.noreply.github.com>
2025-10-15 23:35:09 +0530

    Implement program to find maximum of two numbers

---
+++ ternery2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int num1,num2;
+
+    printf("enter the first number :");
+    scanf("%d",&num1);
+
+    printf("enter the second number ;");
+    scanf( "%d", &num2);
+
+    int max=num1>num2?num1:num2;
+    printf("max num is %d", max);
+
+}
