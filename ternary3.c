7f247ea238edf905867c724fb48c7ed1b4486f48
AJINKYAPATI <158836219+AJINKYAPATI@users.noreply.github.com>
2025-10-15 23:36:00 +0530

    Implement maximum number finder using ternary operators
    Added a program to find the maximum of five numbers using ternary operators.

---
+++ ternary3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,num3,num4,num5;
+    printf("enter the first number :");
+    scanf("%d",&num1);
+
+    printf("enter the second number :");
+    scanf( "%d", &num2);
+
+    
+    printf("enter the third number :");
+    scanf("%d",&num3);
+
+    printf("enter the fourth number :");
+    scanf( "%d", &num4);
+
+    
+    printf("enter the fifth number :");
+    scanf("%d",&num5);
+
+    int max;
+    max = num1>num2?num1:num2;
+    max = max>num3?max:num3;
+    max = max>num4?max:num5;
+
+    printf("maximum number is : %d",max);
+}
