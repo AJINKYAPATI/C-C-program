232160fec6456024c012db0a51894a860d53ccf9
AJINKYAPATI <158836219+AJINKYAPATI@users.noreply.github.com>
2025-10-16 23:22:55 +0530

    Add prime number checker in prime.c

---
+++ prime.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    
+    int a=2,num,flag=0;
+    printf("enter the number");
+    scanf("%d",&num);
+    while(a<=num/2){
+        if(num%a==0){
+            flag=1;
+            break;
+        }
+        a++;
+    }
+    
+    printf(flag?"not prime number" : "prime number");
+    
+    return 0;
+}
